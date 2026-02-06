44542588f08e6fab7a9ac8a91629bcc153db03db
João Pedro <joaopedropereira2209@gmail.com>
2026-02-05 21:05:10 -0300

    initial commit

---
+++ src/main/java/com/io/github/pedroolivsz/desafiojuniorbackendtodolist/DesafioJuniorBackendTodolistApplication.java
@@ -0,0 +1,13 @@
+package com.io.github.pedroolivsz.desafiojuniorbackendtodolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioJuniorBackendTodolistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioJuniorBackendTodolistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/io/github/pedroolivsz/desafiojuniorbackendtodolist/DesafioJuniorBackendTodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.io.github.pedroolivsz.desafiojuniorbackendtodolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioJuniorBackendTodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
