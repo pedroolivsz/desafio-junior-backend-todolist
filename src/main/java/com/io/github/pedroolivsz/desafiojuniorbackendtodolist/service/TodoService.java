c02beb304d2106e21ff8acf03eaf369488b84eea
João Pedro <joaopedropereira2209@gmail.com>
2026-02-05 21:41:50 -0300

    feat(TodoService): implementation of dependency injection via the constructor

--- src/main/java/com/io/github/pedroolivsz/desafiojuniorbackendtodolist/service/TodoService.java
+++ src/main/java/com/io/github/pedroolivsz/desafiojuniorbackendtodolist/service/TodoService.java
@@ -1,7 +1,16 @@
 package com.io.github.pedroolivsz.desafiojuniorbackendtodolist.service;
 
+import com.io.github.pedroolivsz.desafiojuniorbackendtodolist.entity.Todo;
+import com.io.github.pedroolivsz.desafiojuniorbackendtodolist.repository.TodoRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class TodoService {
+    private TodoRepository todoRepository;
+
+    public TodoService(TodoRepository todoRepository) {
+        this.todoRepository = todoRepository;
+    }
 }
